d7abcedaa49add559ea4bd3ec4fc904bd0c1883e
Sanjay Patel <Got2surf2@users.noreply.github.com>
2026-03-10 11:45:03 -0400

    force fikes up

--- sentimetrx/components/creator/CreatorNav.tsx
+++ sentimetrx/components/creator/CreatorNav.tsx
@@ -131,3 +131,6 @@ export default function CreatorNav({
     </div>
   )
 }
+
+
+
--- sentimetrx/components/creator/EditStudyClient.tsx
+++ sentimetrx/components/creator/EditStudyClient.tsx
@@ -105,3 +105,5 @@ export default function EditStudyClient({ study, logoUrl='', orgName='', isAdmin
     </div>
   )
 }
+
+
